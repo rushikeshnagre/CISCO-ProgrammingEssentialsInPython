291f3b93d9069108ef294d185fa84300b09329db
rushikeshnagre <rushikeshnagre@outlook.com>
2021-09-07 13:00:21 +0530

    Added Labwork Files 002, 003, 004

---
+++ Labwork-Programming Essentials/002-LAB-ThePrintFunction.py
@@ -0,0 +1,6 @@
+print("Hello, Python!")
+print("Rushikesh")
+#print(Rushikesh)        #NameError: name 'Rushikesh' is not defined
+#print"Rushikesh"         #SyntaxError: invalid syntax
+print('Rushikesh')
+#print("Hello, Python!") print("Rushikesh")      #SyntaxError: invalid syntax
---
+++ Labwork-Programming Essentials/004-LAB-ThePrintFunctionFormating.py
@@ -0,0 +1,6 @@
+print("\"I'm\"")
+print("\"\"learning\"\"")
+print("\"\"\"Python\"\"\"")
+
+#Labworks-Hint-Answer
+print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
